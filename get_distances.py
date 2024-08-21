91736b6f47d1223ae8385c70501a4ba6435f45c7
alexandriai168 <alexandria.kay.im@gmail.com>
2024-08-21 16:20:47 -0700

    added song's changes to get_distances.  split into separate functions

--- get_distances.py
+++ get_distances.py
@@ -1,4 +1,5 @@
 from Bio.PDB import PDBParser
+improt numpy as np
 
 '''
 returns distances between target protein and list of proteins
@@ -11,8 +12,10 @@ returns distances between target protein and list of proteins
 # pdb_path = "/rcfs/projects/proteometer/alphafold_swissprot_pdb"
 
 
-def find_distances(uniprot_id, target_residue, residue_list, pdb_path):
-    # create parser
+# best metric for distance is get the distance between target and each of the list and average them out 
+
+def find_mean_distances(structure, target_residue, residue_list, pdb_path):
+    # create parser SF: this  parser need to be outside of loops
     parser = PDBParser()
     # read structure from file
     structure = parser.get_structure('PHA-L', '1fat.pdb')
@@ -22,16 +25,20 @@ def find_distances(uniprot_id, target_residue, residue_list, pdb_path):
 
     # this example uses only the first residue of a single chain.
     # it is easy to extend this to multiple chains and residues.
-    for residue1 in chain:
-        for residue2 in chain:
-            if residue1 != residue2:
-                # compute distance between CA atoms
-                try:
+    residue1 = target_residue
+    distances = []
+    for residue2 in residue_list:
+        distances.append(get_pairwise_distance(structure, residue1, residue2))
+        # ceremove  NAs from distances
+    return np.mean(distances)
+
+def get_pairwise_distance(structure, residu_e1, residue_2):
+                    try:
                     distance = residue1['CA'] - residue2['CA']
                 except KeyError:
                     ## no CA atom, e.g. for H_NAG
                     continue
                 if distance < 6:
                     print(residue1, residue2, distance)
-            # stop after first residue
-            break
+    dist = XXX
+    return dist
